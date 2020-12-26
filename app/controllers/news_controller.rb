ea76e7096cabc65e71a9de63c8ed9c883a0b092d
murohi-siyabako <ryun19901208hk@yahoo.co.jp>
2020-12-26 12:23:39 +0900

    記事閲覧機能

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,4 +1,9 @@
 class NewsController < ApplicationController
-  def index
-  end
+  require "open-uri"
+    def index
+        news_api_key = ENV["NEWS_API_KEY"]
+        uri = "http://newsapi.org/v2/top-headlines?country=jp&apiKey=#{news_api_key}"
+        article_serialized = open(uri).read
+        @articles = JSON.parse(article_serialized)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "news#index"
+  resources :news
 end
